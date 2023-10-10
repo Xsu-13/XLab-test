bb0f1af5fceb7b80b57db3e2a78fcd26235c9f28
gafret <d.lemeshko@xlab-game.com>
2023-10-10 16:10:49 +0300

    new

--- Assets/Scripts/GolfScripts/StoneSpawner.cs
+++ Assets/Scripts/GolfScripts/StoneSpawner.cs
@@ -7,7 +7,6 @@ public class StoneSpawner : MonoBehaviour
     public List<GameObject> stones;
     public void Spawn()
     {
-        Debug.Log("Hi");
         int index = Random.Range(0, stones.Count);
         Instantiate(stones[index], transform.position, Quaternion.identity);
     }
